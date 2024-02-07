b4e2f560e7d4e51d63531d2b6660084064f0bc8c
RobertGubo <156349930+RobertGubo@users.noreply.github.com>
2024-02-07 17:36:46 +0100

    Slideshow auf Index.html mit Funfacts

--- script.js
+++ script.js
@@ -5,3 +5,37 @@ toggleButton.addEventListener("click", () =>{
     listContainer.classList.toggle("active")
 })
 
+window.onscroll = function() {scrollFunction()};
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                document.getElementById("topBtn").style.display = "block";
+            } else {
+                document.getElementById("topBtn").style.display = "none";
+            }
+        }
+
+        function topFunction() {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        }
+
+        let slideIndex = 0;
+        const slides = document.querySelectorAll('.slide');
+    
+        function showSlide(n) {
+            slideIndex = (n + slides.length) % slides.length;
+            slides.forEach((slide, index) => {
+                slide.classList.toggle('active', index === slideIndex);
+            });
+        }
+    
+        function prevSlide() {
+            showSlide(slideIndex - 1);
+        }
+    
+        function nextSlide() {
+            showSlide(slideIndex + 1);
+        }
+    
+        showSlide(slideIndex);
